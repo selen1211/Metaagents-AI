a04cc5943142110779d06352602091c7778da706
selen1211 <selen1211x05@gmail.com>
2024-12-26 01:49:48 -0500

    metaagents ai

---
+++ wp-content/litespeed/js/caff7248568a107eab13de550a0f7c41.js
@@ -0,0 +1,2 @@
+jQuery.noConflict();jQuery(function($){$(window).load(function(){$("a[href='#']").bind('click',function(e){e.preventDefault();var ref=$('#'+$(this).attr('rel'));var topTo=ref.offset().top-50;console.log(topTo);window.scrollTo({top:topTo,behavior:'smooth'})})})})
+;
